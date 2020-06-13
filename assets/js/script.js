2c07186a302601a09967775ac05464d12cf1f4c2
Ethan Torres <torresethan99@gmail.com>
2020-06-12 22:42:38 -0500

    Taskinator beta build version 1.0.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
+
+var formEl = document.querySelector("#task-form");
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 console.log(buttonEl);
 
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    listItemEl.textContent = "This is a new task";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
